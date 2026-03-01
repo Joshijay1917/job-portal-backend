d02bca67259c6e92c9201de7c5e2c1bb22d36bb6
Joshijay1917 <jayjoshi1912007@gmail.com>
2026-03-01 20:06:32 +0530

    feat: now origins allowed from env variables

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,19 @@ import authRouter from "./routes/auth.routes.js"
 import cors from 'cors'
 import jobRouter from "./routes/job.routes.js"
 import userRouter from "./routes/user.routes.js"
+import { envConfig } from "./config/envConfig.js"
 
 const app = express()
+const allowedOrigins = envConfig()
 
+app.use(cors({ origin: function(origin, callback) {
+    if(!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+    } else {
+        callback(new Error('Not allowed by CORS:' + origin))
+    }
+}, credentials: true }))
 app.use(express.json())
-app.use(cors({ origin: ['http://localhost:5173', process.env.CORS!], credentials: true }))
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.set('view engine', 'ejs');
